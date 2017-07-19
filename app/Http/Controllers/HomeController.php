06e6c4bcc7770a4abd3e0ff8c63c0469b7657ac9
Juan Carlos Orrego <jcorrego@gmail.com>
2017-07-19 08:34:26 -0500

    Text changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
           'name'        => 'Apologética Juvenil',
           'thumbnail'   => '/images/banners/apologetica_juvenil_720.jpg',
           'image'       => '/images/banners/apologetica_juvenil.jpg',
-          'subtitle'    => '-',
+          'subtitle'    => 'Mentenerse firmes',
           'description' => 'Cómo ayudar a nuestros jóvenes y adolescentes a mantenerse firmes en su fe y tener argumentos para defenderla? <br><br>Proveemos y elaboramos talleres, estudios, herramientas de apoyo y principios bíblicos para que los jóvenes pre- universitarios y universitarios reciban las bases y la forma en que pueden conocer y abordar temas como la homosexualidad, ideología de género, el matrimonio igualitario, la política, los peligros de la web, etc y puedan mantenerse firmes en la fe en Cristo en medio del pensamiento de este siglo. (Investigación profunda).',
         ];
         $services[]  = [
@@ -36,23 +36,23 @@ class HomeController extends Controller
           'name'        => 'Cápsulas bíblicas juveniles',
           'thumbnail'   => '/images/banners/capsulas_bibilicas_juveniles_720.jpg',
           'image'       => '/images/banners/capsulas_bibilicas_juveniles.jpg',
-          'subtitle'    => '-',
-          'description' => 'Elaboramos y distribuimos enseñanzas, estudios y reflexiones Prácticas, Profundas y Posibles, de fácil lectura y comprensión para los adolescentes y jóvenes de las iglesias y lo no creyentes. Estas son de soporte y ayuda en los ministerios juveniles para obtener materiales de enseñanza ordenados y consecutivos para sus reuniones y escuelas bíblicas.',
+          'subtitle'    => 'Prácticas, Profundas y Posibles',
+          'description' => 'Elaboramos y distribuimos enseñanzas, estudios y reflexiones Prácticas, Profundas y Posibles, de fácil lectura y comprensión para los adolescentes y jóvenes de las iglesias y los no creyentes. Estas son de soporte y ayuda en los ministerios juveniles para obtener materiales de enseñanza ordenados y consecutivos para sus reuniones y escuelas bíblicas.',
         ];
         $services[]  = [
           'id'          => '3',
           'name'        => 'H2J',
           'thumbnail'   => '/images/banners/h2j_720.jpg',
           'image'       => '/images/banners/h2j.jpg',
-          'subtitle'    => '-',
-          'description' => '(Hermenéutica y Homilética juvenil) Enseñamos y entrenamos a líderes de jóvenes en preparar la enseñanza Biblica y trasmitirla de manera creativa y relevante para adolescentes y jovenes. Proveemos herramientas didácticas y contextualizadas para la enseñanza en el ministerio juvenil.',
+          'subtitle'    => 'Hermenéutica y Homilética juvenil',
+          'description' => 'Enseñamos y entrenamos a líderes de jóvenes en preparar la enseñanza Biblica y trasmitirla de manera creativa y relevante para adolescentes y jovenes. Proveemos herramientas didácticas y contextualizadas para la enseñanza en el ministerio juvenil.',
         ];
         $services[]  = [
           'id'          => '4',
           'name'        => 'Keepers School',
           'thumbnail'   => '/images/banners/keepers_school_720.jpg',
           'image'       => '/images/banners/keepers_school.jpg',
-          'subtitle'    => '-',
+          'subtitle'    => 'Enseñanza Biblica',
           'description' => 'Apoyamos procesos serios de pastoral juvenil, énfasis espiritual y de valores en los colegios y entidades educativas que así lo requieran. Proveemos y desarrollamos de manera dinámica y contextualizada la enseñanza Biblica a los jóvenes y adolescentes dentro del colegio. <br><br>Capacitamos el cuerpo de docentes en temas referentes al trabajo y ministerio juvenil en los colegios en temas como el matoneo, adicciones, homosexualidad y muchos más con herramientas bíblicas que respalden el levantar una generación que tema al Señor.',
         ];
         $services[]  = [
@@ -68,7 +68,7 @@ class HomeController extends Controller
           'name'        => 'The Good Young Life',
           'thumbnail'   => '/images/banners/the_good_young_life_720.jpg',
           'image'       => '/images/banners/the_good_young_life.jpg',
-          'subtitle'    => 'Renovation and Restoration',
+          'subtitle'    => 'Renovación y Restauración',
           'description' => 'Este es un seminario y taller bíblico didáctico donde llevamos a los jóvenes y adolescentes a comprender y asumir lo que es el verdadero significado de la juventud. Enfocamos la identidad, el propósito, el lugar del joven en su familia, el manejo de relaciones, sus principales luchas y vacíos entendiendo sus causas y la forma en que pueden ser suplidos por el Señor.  La dinámica y estilo de este seminario - taller permite ver los ejemplos directos de jóvenes y adolescentes en la Biblia en su proceso de vida llevando al participante a identificarse con ellos para comprobar la obra y la intervención del Señor.'
         ];
         $materials   = [];
